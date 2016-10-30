be13fc750410998cf2b183baa611ee3dff732b5d
woogii <jhsw0201@naver.com>
2016-10-30 18:50:53 +0900

    feat : Implement findMToLastElement method in two ways

--- Chap2_LinkedList/main.cpp
+++ Chap2_LinkedList/main.cpp
@@ -21,15 +21,13 @@ StackError::StackError(std::string errorMessage) {
     std::cout << errorMessage << std::endl;
 }
 
+StackError::~StackError() {
+    
+}
+
+
 class Stack {
-    
-public:
-    Stack();
-    ~Stack();
-    void push(void* data);
-    void *pop();
-    void printAllElementsInStack();
-    
+ 
     protected :
     
     typedef struct Element {
@@ -38,6 +36,18 @@ public:
     };
     
     Element* head;
+
+    
+public:
+    Stack();
+    ~Stack();
+    void push(void* data);
+    void *pop();
+    void printAllElementsInStack();
+    
+    Element *findMToListElement(Element *head, int m);
+    int findMToListElementByRecursive(Element *head, int m);
+    Element* getHead();
     
 };
 
@@ -84,12 +94,60 @@ void* Stack::pop() {
     }
 }
 
+Stack::Element* Stack::getHead() {
+    return this->head;
+}
+
 void Stack::printAllElementsInStack() {
 
     while(head) {
         std::cout << *((int*)(head->data)) << std::endl;
         head = head->next;
     }
+}
+
+Stack::Element* Stack::findMToListElement(Stack::Element *head, int m) {
+    
+    Stack::Element *current = head;
+    Stack::Element *mBehind;
+    
+    if (head == NULL) {
+        return NULL;
+    }
+    
+    for( int i =0; i < m; i++) {
+        if (current->next) {
+            current = current->next;
+        } else {
+            return NULL;
+        }
+    }
+    
+    mBehind = head;
+    
+    while(current->next) {
+        current = current->next;
+        mBehind = mBehind->next;
+    }
+    
+    return mBehind;
+}
+
+
+
+int Stack::findMToListElementByRecursive(Stack::Element *current, int m) {
+    
+    int index = 0;
+    
+    if (current->next != NULL) {
+        index = findMToListElementByRecursive(current->next, m) + 1;
+    }
+    
+    if (index == m) {
+        std::cout << *((int*)current->data) << std::endl;
+    }
+    
+    return index;
 }
 
 int main(int argc, const char * argv[]) {
@@ -103,9 +161,13 @@ int main(int argc, const char * argv[]) {
     stack.push(&arr[3]);
     stack.push(&arr[4]);
     
-    for(int i=0; i<sizeof(arr)/sizeof(int); i++) {
-        std::cout << *((int*)stack.pop()) << std::endl;
-    }
+//    for(int i=0; i<sizeof(arr)/sizeof(int); i++) {
+//        std::cout << *((int*)stack.pop()) << std::endl;
+//    }
+    
+    // std::cout << *((int*)(stack.findMToListElement(stack.getHead(), 4)->data)) << std::endl;
+    
+    stack.findMToListElementByRecursive(stack.getHead(), 3);
     
     return 0;
 }
