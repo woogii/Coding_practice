14a9e80a5f6c5086ea7ce13ee1acaa68bd25dca8
woogii <jhsw0201@naver.com>
2017-03-25 16:38:54 +0900

    feat: Implement Swift Merge Sort

--- ArrayAndString.playground/Contents.swift
+++ ArrayAndString.playground/Contents.swift
@@ -4,7 +4,7 @@ import UIKit
 
 
 var dict = Dictionary<Character,Int>()
-var myDictionary: [Int: [Character: Int]] = [:]
+var myDictionary: [Int: [Character: Int]] = [0:[:], 1:[:],2:[:],3:[:], 4:[:]]
 
 extension Character
 {
@@ -22,7 +22,7 @@ var test = "total"
 for i in 0..<test.characters.count {
   //print(char)
   
-    var dict = myDictionary[i] as! [Character:Int]
+  if var dict = myDictionary[i] {
     
     if dict.contains(where: {return $0.key == test[test.index(test.startIndex, offsetBy: i)] }) {
       if let value = dict[test[test.index(test.startIndex, offsetBy: i)]] {
@@ -31,6 +31,8 @@ for i in 0..<test.characters.count {
     } else {
       dict[test[test.index(test.startIndex, offsetBy: i)]] = 1
     }
+    
+  }
   
 }
 
