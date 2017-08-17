de857f332f241f20823dce924d5066c921fd4c81
woogii <jhsw0201@naver.com>
2017-08-17 18:49:46 +0200

    feat : Add another solution for the palindrome problem

--- CountingPalindromes.playground/Contents.swift
+++ CountingPalindromes.playground/Contents.swift
@@ -7,31 +7,54 @@ var str = "Hello, playground"
 
 let sentence = "madam anna kayak notapalindrome anna Civic racecar"
 
+extension String {
+  
+  func charAtIndex(index:Int)->Character {
+    return self[self.index(self.characters.startIndex, offsetBy: index)]
+  }
+  
+}
 
 func allPalindromCounts(sentence:String) {
   
   var result:[String:Int] = [:]
   let wordList = sentence.components(separatedBy: " ")
   
-  for word in wordList {
+  wordList.forEach { word in
     
-    let reversedWord = String(word.characters.reversed())
+    print(word)
     
-    if word.caseInsensitiveCompare(reversedWord) == ComparisonResult.orderedSame {
+    if isPalindrom(word:word) {
       
-      if let wordCount = result[word]  {
-        result[word] = wordCount + 1
-      } else {
-        result[word] = 1
-      }
+      let wordCount = result[word] ?? 0
+      result[word]  = wordCount + 1
+      
     }
-    
     
   }
   
   print(result)
 }
 
+func isPalindrom(word:String) -> Bool {
+  
+  let characters = Array(word.lowercased().characters)
+  var startIndex = 0
+  var endIndex = word.characters.count - 1
+  
+  while startIndex < endIndex {
+    
+    if characters[startIndex] != characters[endIndex] {
+      return false
+    }
+    
+    startIndex += 1
+    endIndex -= 1
+    
+  }
+  
+  return true
+}
 
 
 allPalindromCounts(sentence: sentence)
