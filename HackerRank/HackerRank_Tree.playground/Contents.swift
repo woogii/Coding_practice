267709d34b6d6d2fc7f19484bc28f1ee35d1245d
woogii <jhsw0201@naver.com>
2017-08-28 19:36:13 +0200

    Chore : reimplement tree height calculation logic and tree insert function

--- HackerRank/HackerRank_Tree.playground/Contents.swift
+++ HackerRank/HackerRank_Tree.playground/Contents.swift
@@ -52,8 +52,6 @@ class Tree {
     }
     
   }
-  
-  
 }
 
 var root: Node?
@@ -68,4 +66,79 @@ for _ in 0..<t {
 //tree.insert(root: root, data: 1)
 //tree.insert(root: root, data: 7)
 print(tree.getHeight(root: root))
+
+
+
+class BinaryNode {
+  
+  var value:Int
+  var left:BinaryNode?
+  var right:BinaryNode?
+  
+  init(value:Int) {
+    self.value = value
+  }
+}
+
+
+
+
+class BinaryTree {
+  
+  func insert(root:BinaryNode?, value:Int)->BinaryNode? {
+    
+    guard root != nil else {
+      return BinaryNode(value: value)
+    }
+    
+    if value <= root!.value {
+      root?.left = insert(root: root?.left, value: value)
+    } else if value > root!.value {
+      root?.right = insert(root: root?.right, value: value)
+    }
+    
+    return root
+  }
+  
+  func getHeight(root:BinaryNode?)->Int {
+    
+    if root == nil {
+      return -1
+    }
+    
+    let leftHeight = getHeight(root: root?.left)
+    let rightHeight = getHeight(root: root?.right)
+    return max(leftHeight, rightHeight) + 1
+    
+  }
+  
+  
+}
+
+
+var binaryTree = BinaryTree()
+
+let binaryTreeRoot = binaryTree.insert(root: nil, value: 4)
+
+binaryTree.insert(root: binaryTreeRoot, value: 2)
+binaryTree.insert(root: binaryTreeRoot, value: 7)
+binaryTree.insert(root: binaryTreeRoot, value: 1)
+binaryTree.insert(root: binaryTreeRoot, value: 3)
+binaryTree.insert(root: binaryTreeRoot, value: 6)
+
+print(binaryTree.getHeight(root: binaryTreeRoot))
+
+print("===========")
+func inOrderTraverse(root:BinaryNode?) {
+  
+  if root != nil {
+    print(root?.value ?? 0)
+    inOrderTraverse(root: root?.left)
+    inOrderTraverse(root: root?.right)
+  }
+  
+  return
+}
+
+inOrderTraverse(root:binaryTreeRoot)
 
